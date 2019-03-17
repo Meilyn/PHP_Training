306d0efce7afc483834f361f164ca8418e9544bb
Meilyn <[andrade.meiyn@gmail.com]>
2019-03-17 18:50:29 +0100

    controleur

---
+++ Controleur/index.php
@@ -0,0 +1,43 @@
+<?php 
+	
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Les Fonctions</title>
+	<!-- Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+	<meta name="description" content="Exercices PHP">
+	<meta name="keywords" content="HTML,CSS,PHP, Bootstrap">
+	<meta name="author" content="[MafiaGirl || Angry Creative]">
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<nav class="navbar navbar-dark bg-dark">
+		<i class="fas fa-user-tie text-align-center"> Controleur</i>
+	</nav>
+	<div class="jumbotron">
+		<h1>CONTROLEUR</h1>
+	</div>
+		<div class="container">
+			<h3 id="inv">Exercice N°1</h3><hr>	
+			<p class="text-justify">
+			Utilise une fonction qui mette la première lettre de l'argument en majuscule. Par ex: si l'input de la fonction est "emile", l'output sera "Emile". <br>
+				<code>
+					<?php 
+					 
+					?>
+				</code>
+			</p>					
+		</div>	
+</body>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</html>
